f673db75e32f981dd21cef043214e78a0d9f6076
Kuru Juhani <juhani.kuru@lapinamk.fi>
2022-04-13 15:02:45 +0300

    remove code

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -10,8 +10,8 @@ from models import User
 
 
 # /api/login
-@jwt_required(refresh=True)
-#jorma = 'jorma'
+
+
 def login_route_handler():
     if request.method == 'POST':
         request_body = request.get_json()
@@ -31,24 +31,7 @@ def login_route_handler():
     
     
 
-""" class LoginRouteHandler(MethodView):
-    def post(self):
-        request_body = request.get_json()
-        username = request_body['username']
-        password = request_body['password']
-        user = User.get_by_username(username)
-        password_ok = sha256.verify(password, user.password)
-        if password_ok is True:
-            access_token = create_access_token(
-                str(user._id), 
-                additional_claims={'username': user.username, 'role': user.role})
-            
-            return jsonify(access_token=access_token)
-        raise NotFound(message='User not found')
-    
-    @jwt_required(refresh=True)
-    def patch(self):
-        pass """
+
 
         
         
